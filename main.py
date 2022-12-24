6908faf881f7dc9b6255268b95fcbaa5c3f12d7a
Jose Sanchez <jsanxez7@gmail.com>
2022-12-24 16:01:13 -0500

    add: showing weather info in template

--- main.py
+++ main.py
@@ -23,12 +23,17 @@ class Item(BaseModel):
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-
-@app.get("/{zipcode}")
-async def get_zipcode_weather(zipcode: str):
-    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?zip={zipcode},{default_country}&appid={api_key}")
-    requested_weather = weather_data.json()
-    return requested_weather
+@app.get("/weather/", response_class=HTMLResponse)
+async def get_zipcode_weather(request: Request, zipcode: str):
+    weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?zip={zipcode},{default_country}&units=metric&appid={api_key}")
+    weather_data = weather_data.json()
+    requested_weather = {"city": weather_data['name'],
+                          "temperature": weather_data['main']['temp'],
+                          "description": weather_data['weather'][0]['description'],
+                          "humidity": weather_data['main']['humidity'],
+                          "wind": weather_data['wind']['speed'],
+                          }
+    return templates.TemplateResponse("index.html", {"request": request, "weather": requested_weather})
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
